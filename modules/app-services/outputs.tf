12e67eb402694367d6265a35fdee68bc87ff257a
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2024-07-21 20:29:27 -0300

    fix module url output

--- modules/app-services/outputs.tf
+++ modules/app-services/outputs.tf
@@ -1,3 +1,3 @@
 output "appservice_address" {
-  value = "App Service URL: https://${azurerm_linux_web_app.main.default_hostname}/swagger"
+  value = "App Service URL: https://${azurerm_linux_web_app.main.default_hostname}"
 }
